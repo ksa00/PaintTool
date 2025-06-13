4e55041c19f292e82802e6a697b673b1f3e3b764
Ksa <230587@jc-21.jp>
2025-06-13 15:10:49 +0900

    painttool sample

--- app/src/main/java/com/example/painttool/MainActivity.java
+++ app/src/main/java/com/example/painttool/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.painttool;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //ラジオボタンイベントの実装
+        RadioGroup radioGroup=findViewById(R.id.colorGrp);
+        RadioGroup.setOnCkeckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group,int checkedId){
+                RadioButton checked=findViewById(checkedId);
+                //ボタンクラスから選択されている色情報の文字を取得
+                String checkedColor=checked.getText().toString();
+                if(checkedColor.equals("Red")){
+                    //paintviewの色設定機能の呼び出し
+                }
+            }
+        });
     }
+    //取り消しボタンイベントの実装
+    //Paintviewの取り消し機能の呼び出し
+    //消去ボタンイベントの実装
+    //Paintviewの消去機能の呼び出し
+    //保存ボタンイベントの実装
+    //paintview保存機能の呼び出し
+
 }
--- app/src/main/java/com/example/painttool/PaintView.java
+++ app/src/main/java/com/example/painttool/PaintView.java
@@ -1,16 +1,29 @@
 package com.example.painttool;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.format.DateFormat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class PaintView extends View {
     private Context _context;
@@ -26,6 +39,7 @@ public class PaintView extends View {
     }
 
   //描画メソッドの上書き
+    //この関数はシステムコントロール専用です
     @Override
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
@@ -70,7 +84,62 @@ public class PaintView extends View {
                 break;
         }
         //再描画（画面破棄）
+        invalidate();
+
         return true;
     }
 
+    //取り消し機能の実装
+    public void undo(){
+        //最後に書かれた線の消去
+        //再描画
+    }
+    //画面消去機能実装
+    public void clear(){
+        //全ての線情報の消去
+        //再描画
+    }
+    //線の色を更新するセッタ
+    public void setColor(Color color){
+        //色情報を線に設定
+    }
+    //保存機能の実装
+    public void SaveFiles(){
+        //bitmapオブジェクトの初期化
+        Bitmap bitmap=Bitmap.createBitmap(this.getWidth(),this.getHeight(),Bitmap.Config.ARGB_8888);
+        //描画対象のキャンバスにbitmap描画情報を設定
+        Canvas bitmapCanvas=new Canvas(bitmap);
+        //全ての線情報を今回用意したCanvasに描画＝onDraw()と処理が被ります
+
+        //MediaStoreAPIを使用
+        try{
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.Q){
+                //ギャラリロールへの画像登録
+                ContentResolver resolver=_context.getContentResolver();
+                ContentValues values=new ContentValues();
+
+                //ファイル名を決定(管理番号としてミリ秒を使用)
+                long milliSec=System.currentTimeMillis();
+                String fileName= DateFormat.format("yyyy-MM-dd_kk-mm-ss",milliSec).toString();
+
+                //登録情報の設定
+                values.put(MediaStore.MediaColumns.DISPLAY_NAME,"Image_PT_"+fileName+".jpeg");
+                values.put(MediaStore.MediaColumns.MIME_TYPE,"image/jpeg");
+                values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES+ File.separator+"PaintTool");
+
+                //保存先のuriを取得
+                Uri imageURI=resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+
+                //保存先Uriにファイルを保存
+                OutputStream fos=resolver.openOutputStream(Objects.requireNonNull(imageURI));
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100,fos);
+                Objects.requireNonNull(fos);
+
+                Toast.makeText(_context,"Save Successful",Toast.LENGTH_LONG).show();
+            }
+
+        } catch (Exception e) {
+            Toast.makeText(_context,"Save Failed!!!,Because "+e.toString(),Toast.LENGTH_LONG).show();
+        }
+    }
 }
